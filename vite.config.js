87b68003544aec62ab791908cdf955947f9a3358
alexsimonov-ux <alex.simonov@peopleforce.io>
2026-03-11 14:10:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // ВАЖНО: замени 'antigravity' на имя твоего GitHub репозитория
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/pf/',
 })
